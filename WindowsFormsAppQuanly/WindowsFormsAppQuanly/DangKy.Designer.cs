c318a0967337e13ec9b089e0835f50acf011f4e5
hoangson <tranhoangson03@gmail.com>
2023-12-16 00:05:33 +0700

    first commit

---
+++ WindowsFormsAppQuanly/WindowsFormsAppQuanly/DangKy.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace WindowsFormsAppQuanly
+{
+    partial class DangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_TenTaiKhoan = new System.Windows.Forms.TextBox();
+            this.textBox_MatKhau = new System.Windows.Forms.TextBox();
+            this.textBox_XNMatKhau = new System.Windows.Forms.TextBox();
+            this.textBox_Email = new System.Windows.Forms.TextBox();
+            this.button_DangKy = new System.Windows.Forms.Button();
+            this.button_Thoat = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng Ký Tài Khoản";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox_TenTaiKhoan
+            // 
+            this.textBox_TenTaiKhoan.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_TenTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_TenTaiKhoan.Location = new System.Drawing.Point(149, 306);
+            this.textBox_TenTaiKhoan.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_TenTaiKhoan.Name = "textBox_TenTaiKhoan";
+            this.textBox_TenTaiKhoan.Size = new System.Drawing.Size(267, 20);
+            this.textBox_TenTaiKhoan.TabIndex = 6;
+            // 
+            // textBox_MatKhau
+            // 
+            this.textBox_MatKhau.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_MatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_MatKhau.Location = new System.Drawing.Point(149, 360);
+            this.textBox_MatKhau.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_MatKhau.Name = "textBox_MatKhau";
+            this.textBox_MatKhau.Size = new System.Drawing.Size(267, 20);
+            this.textBox_MatKhau.TabIndex = 7;
+            // 
+            // textBox_XNMatKhau
+            // 
+            this.textBox_XNMatKhau.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_XNMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_XNMatKhau.Location = new System.Drawing.Point(149, 414);
+            this.textBox_XNMatKhau.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_XNMatKhau.Name = "textBox_XNMatKhau";
+            this.textBox_XNMatKhau.Size = new System.Drawing.Size(267, 20);
+            this.textBox_XNMatKhau.TabIndex = 8;
+            // 
+            // textBox_Email
+            // 
+            this.textBox_Email.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Email.Location = new System.Drawing.Point(148, 469);
+            this.textBox_Email.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Email.Name = "textBox_Email";
+            this.textBox_Email.Size = new System.Drawing.Size(267, 20);
+            this.textBox_Email.TabIndex = 9;
+            // 
+            // button_DangKy
+            // 
+            this.button_DangKy.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_DangKy.Location = new System.Drawing.Point(119, 523);
+            this.button_DangKy.Margin = new System.Windows.Forms.Padding(4);
+            this.button_DangKy.Name = "button_DangKy";
+            this.button_DangKy.Size = new System.Drawing.Size(272, 41);
+            this.button_DangKy.TabIndex = 10;
+            this.button_DangKy.Text = "Tạo Tài Khoản";
+            this.button_DangKy.UseVisualStyleBackColor = true;
+            this.button_DangKy.Click += new System.EventHandler(this.button_DangKy_Click);
+            // 
+            // button_Thoat
+            // 
+            this.button_Thoat.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Thoat.Location = new System.Drawing.Point(119, 583);
+            this.button_Thoat.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Thoat.Name = "button_Thoat";
+            this.button_Thoat.Size = new System.Drawing.Size(272, 41);
+            this.button_Thoat.TabIndex = 11;
+            this.button_Thoat.Text = "Thoát";
+            this.button_Thoat.UseVisualStyleBackColor = true;
+            this.button_Thoat.Click += new System.EventHandler(this.button_Thoat_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::WindowsFormsAppQuanly.Properties.Resources.email_123_64;
+            this.pictureBox5.Location = new System.Drawing.Point(91, 459);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(35, 31);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 16;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::WindowsFormsAppQuanly.Properties.Resources.khoa;
+            this.pictureBox4.Location = new System.Drawing.Point(89, 404);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(35, 31);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 15;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsAppQuanly.Properties.Resources.khoa;
+            this.pictureBox3.Location = new System.Drawing.Point(90, 349);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(37, 31);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsAppQuanly.Properties.Resources.user1;
+            this.pictureBox2.Location = new System.Drawing.Point(90, 295);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsAppQuanly.Properties.Resources.pngwing_com;
+            this.pictureBox1.Location = new System.Drawing.Point(150, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(186, 163);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(90, 336);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(326, 2);
+            this.panel1.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(91, 387);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(326, 2);
+            this.panel2.TabIndex = 18;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(89, 441);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(326, 2);
+            this.panel3.TabIndex = 19;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Black;
+            this.panel4.Location = new System.Drawing.Point(90, 496);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(326, 2);
+            this.panel4.TabIndex = 19;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Red;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(437, 12);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 20;
+            // 
+            // guna2Elipse2
+            // 
+            this.guna2Elipse2.TargetControl = this;
+            // 
+            // DangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(494, 681);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_Thoat);
+            this.Controls.Add(this.button_DangKy);
+            this.Controls.Add(this.textBox_Email);
+            this.Controls.Add(this.textBox_XNMatKhau);
+            this.Controls.Add(this.textBox_MatKhau);
+            this.Controls.Add(this.textBox_TenTaiKhoan);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "DangKy";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Ký";
+            this.Load += new System.EventHandler(this.DangKy_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_TenTaiKhoan;
+        private System.Windows.Forms.TextBox textBox_MatKhau;
+        private System.Windows.Forms.TextBox textBox_XNMatKhau;
+        private System.Windows.Forms.TextBox textBox_Email;
+        private System.Windows.Forms.Button button_DangKy;
+        private System.Windows.Forms.Button button_Thoat;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse2;
+    }
+}
---
+++ WindowsFormsAppQuanly/WindowsFormsAppQuanly/DangKy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsAppQuanly
+{
+    public partial class DangKy : Form
+    {
+        public DangKy()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        public bool CheckAccount(string ac)//check mat khau va ten tai khoan
+        {
+            return Regex.IsMatch(ac,"^[a-zA-Z0-9]{6,24}$");
+        }
+        public bool CheckEmail(string em)//check Email
+        {
+            return Regex.IsMatch(em, @"^[a-zA-Z0-9_.]{3,20}@gmail.com(.vn|)$");
+        }
+        Modify modify=new Modify();
+        private void button_DangKy_Click(object sender, EventArgs e)
+        {
+            string tentk = textBox_TenTaiKhoan.Text;
+            string matkhau = textBox_MatKhau.Text;
+            string xnmatkhau = textBox_XNMatKhau.Text;
+            string email = textBox_Email.Text;
+            if (!CheckAccount(tentk)) {MessageBox.Show("Vui lòng nhập tên tài khoản dài 6-24 ký tự, với các ký tự chữ và số, chữ hoa và chữ thường !");return;}
+            if (!CheckAccount(matkhau)) { MessageBox.Show("Vui lòng nhập mật khẩu dài 6-24 ký tự, với các ký tự chữ và số, chữ hoa và chữ thường !"); return; }
+            if(xnmatkhau != matkhau) { MessageBox.Show("Vui lòng xác nhận mật khẩu chính xác !!");return; }
+            if (!CheckEmail(email)) { MessageBox.Show("Vui lòng nhập đúng định dạng Email !!");return; }
+            if (modify.TaiKhoans("Select * from NHANVIEN where Email = '" + email + "' ").Count !=0)
+            {
+                MessageBox.Show("Email này đã được đăng ký vui lòng sử dụng Email khác !!!");
+                return;
+            }
+            try { 
+                string query = "Insert into NHANVIEN values ('"+tentk+"','" + matkhau + "','" + email + "')";
+                modify.Command(query);
+            }
+            catch {
+                MessageBox.Show("Email này đã được đăng ký vui lòng sử dụng Email khác !!!");
+            }
+
+        }
+
+        private void button_Thoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void DangKy_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
